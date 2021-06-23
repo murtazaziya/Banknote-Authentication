2110d04d1749313a666e96c0f7251a3ad8560fef
murtazaziya <83747330+murtazaziya@users.noreply.github.com>
2021-06-23 10:24:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('bank_note_auth.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    if output == 1:
+        return render_template('bank_note_auth.html', prediction_text='Original')
+    else:
+        return render_template('bank_note_auth.html', prediction_text='Fake')
+
+if __name__ == "__main__":
+    app.run(debug=True)
